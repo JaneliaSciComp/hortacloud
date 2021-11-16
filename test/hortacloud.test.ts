66dddcb7c002a2988589d82c37e3bbd8e64046df
Konrad Rokicki <rokickik@janelia.hhmi.org>
2021-11-16 10:04:11 -0500

    Initial commit

---
+++ test/hortacloud.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as Hortacloud from '../lib/hortacloud-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/hortacloud-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Hortacloud.HortacloudStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
