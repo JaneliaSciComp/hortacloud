49cfd827a22e34260b700d795421be8a0ff93a83
porterbot <porterrobot@gmail.com>
2022-01-18 14:47:45 -0500

    added basic placeholders for Cfn appstream

--- src/hortacloud-appstream.ts
+++ src/hortacloud-appstream.ts
@@ -1,5 +1,6 @@
 import { Construct } from 'constructs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as appstream from 'aws-cdk-lib/aws-appstream';
 
 import { HortaCloudVPC } from './hortacloud-vpc';
 
@@ -11,5 +12,60 @@ export class HortaCloudAppstream extends Construct {
               id: string,
               hortaVpc: HortaCloudVPC) {
     super(scope, id);
+    // still need to add vpc configuration from main stack
+    const hortaCloudFleet = new appstream.CfnFleet(this, 'MyCfnFleet', {
+      instanceType: 'stream.graphics-pro.4xlarge',
+      name: 'name',
+      computeCapacity: {
+        desiredInstances: 123,
+      },
+      displayName: 'HortaCloudAppstreamFleet',
+      enableDefaultInternetAccess: false,
+      fleetType: 'On-demand',
+      maxConcurrentSessions: 5,
+      maxUserDurationInSeconds: 960
+    });
+
+    const hortaCloudStack = new appstream.CfnStack(this, 'MyCfnStack',{
+      accessEndpoints: [{
+        endpointType: 'STREAMING',
+        vpceId: hortaVpc.vpc.vpcId,
+      }],
+      applicationSettings: {
+        enabled: false
+      },
+      displayName: 'HortaCloudAppstreamStack',
+      name: 'HortaCloudAppstreamStack',
+    });
+
+    const hortaCloudStackFleetAssociation = new appstream.CfnStackFleetAssociation(this, 'HortaCloudStackFleet', {
+      fleetName: 'HortaCloudAppstreamFleet',
+      stackName: 'HortaCloudAppstreamStack',
+    });
+
+    const hortaCloudImageBuilder = new appstream.CfnImageBuilder(this, 'HortaCloudImageBuilder', {
+      instanceType: 'stream.graphics-pro.4xlarge',
+      name: 'HortaCloudImageBuilder',
+
+      accessEndpoints: [{
+        endpointType: 'STREAMING',
+        vpceId: hortaVpc.vpc.vpcId,
+      }],
+      displayName: 'HortaCloudImageBuilder',
+    });
+
+    /*const hortaCloudApp = new appstream.CfnApplication(this, 'HortaCloudAppstream', {
+      appBlockArn: 'appBlockArn',
+      iconS3Location: {
+        s3Bucket: 's3Bucket',
+        s3Key: 's3Key',
+      },
+      instanceFamilies: ['instanceFamilies'],
+      launchPath: 'launchPath',
+      name: 'name',
+      platforms: ['platforms'],
+
+    });*/
+
   }
 }
