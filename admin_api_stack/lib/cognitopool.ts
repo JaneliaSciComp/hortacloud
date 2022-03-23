42775f5b4287ec44453147ce58d798a8ac6f9b8c
Jody Clements <jody@neomorphic.co.uk>
2022-03-23 10:50:00 -0400

    Improves invite email template.
    The password and login urls are forced onto new lines, preventing them
    from being lost in a word soup.

--- admin_api_stack/lib/cognitopool.ts
+++ admin_api_stack/lib/cognitopool.ts
@@ -1,9 +1,9 @@
-import * as cdk from 'aws-cdk-lib';
+import * as cdk from "aws-cdk-lib";
 import * as cognito from "aws-cdk-lib/aws-cognito";
 import { Construct } from "constructs";
-import { getHortaCloudConfig } from '../../common/hortacloud-common';
+import { getHortaCloudConfig } from "../../common/hortacloud-common";
 
-const adminGroupName = 'admins';
+const adminGroupName = "admins";
 
 export class CognitoPool extends Construct {
   public readonly pool: cognito.UserPool;
@@ -16,7 +16,7 @@ export class CognitoPool extends Construct {
 
     const adminBucketUrl = `${props.hortaCloudOrg}-hc-webadmin-${props.hortaStage}.s3-website-${process.env.AWS_REGION}.amazonaws.com`;
 
-    this.pool = new cognito.UserPool(this, 'HortaCloudUsers', {
+    this.pool = new cognito.UserPool(this, "HortaCloudUsers", {
       selfSignUpEnabled: false,
       signInCaseSensitive: false,
       signInAliases: {
@@ -26,67 +26,84 @@ export class CognitoPool extends Construct {
       standardAttributes: {
         email: {
           required: true,
-          mutable: false,
+          mutable: false
         }
       },
       // TODO: change the fromEmail address to a less opaque domain name.
       // The no-reply@verificationemail.com domain does not inspire confidence.
       userInvitation: {
-        emailSubject: 'HortaCloud Invite',
-        emailBody: `Hello {username}, you have been invited to work with our HortaCloud service. Your temporary password is '{####}'. Please login at ${adminBucketUrl}`,
-        smsMessage: "Hello {username}, you have been invited to work with our HortaCloud service. Your temporary password is '{####}'.",
+        emailSubject: "HortaCloud Invite",
+        emailBody: `Hello {username}, you have been invited to work with our HortaCloud service. <br/>
+        Your temporary password is '{####}'.<br/>
+        Please login at ${adminBucketUrl}`,
+        smsMessage:
+          "Hello {username}, you have been invited to work with our HortaCloud service. Your temporary password is '{####}'."
       },
       autoVerify: { email: true },
-      accountRecovery: cognito.AccountRecovery.EMAIL_ONLY,
+      accountRecovery: cognito.AccountRecovery.EMAIL_ONLY
     });
 
     // this generates the clientId that will be used by the admin site
-    this.userPoolClient = new cognito.UserPoolClient(this, "AdminUserPoolClient", {
-      userPool: this.pool
-    });
+    this.userPoolClient = new cognito.UserPoolClient(
+      this,
+      "AdminUserPoolClient",
+      {
+        userPool: this.pool
+      }
+    );
 
     // add the admins group
-    const adminGroup = new cognito.CfnUserPoolGroup(this, 'AdminsUserPoolGroup', {
-      userPoolId: this.pool.userPoolId,
-      // the properties below are optional
-      description: 'Adminsitrative users',
-      groupName: adminGroupName,
-      precedence: 0,
-      // roleArn: 'roleArn',
-    });
-
-    if (process.env.ADMIN_USER_EMAIL) {
-
-      // add the default admin user for first access.
-      const adminUser = new cognito.CfnUserPoolUser(this, 'DefaultAdminUserPoolUser', {
+    const adminGroup = new cognito.CfnUserPoolGroup(
+      this,
+      "AdminsUserPoolGroup",
+      {
         userPoolId: this.pool.userPoolId,
         // the properties below are optional
-        username: process.env.ADMIN_USER_EMAIL,
-        userAttributes: [
-          {
-            name: 'email',
-            value: process.env.ADMIN_USER_EMAIL
-          },
-          {
-            name: 'email_verified',
-            value: 'True'
-          },
-        ]
-      });
-
-      const attachAdminUser = new cognito.CfnUserPoolUserToGroupAttachment(this, 'AdminUsertoAdminGroupAttachment', {
+        description: "Adminsitrative users",
         groupName: adminGroupName,
-        username: process.env.ADMIN_USER_EMAIL,
-        userPoolId: this.pool.userPoolId,
-      });
+        precedence: 0
+        // roleArn: 'roleArn',
+      }
+    );
+
+    if (process.env.ADMIN_USER_EMAIL) {
+      // add the default admin user for first access.
+      const adminUser = new cognito.CfnUserPoolUser(
+        this,
+        "DefaultAdminUserPoolUser",
+        {
+          userPoolId: this.pool.userPoolId,
+          // the properties below are optional
+          username: process.env.ADMIN_USER_EMAIL,
+          userAttributes: [
+            {
+              name: "email",
+              value: process.env.ADMIN_USER_EMAIL
+            },
+            {
+              name: "email_verified",
+              value: "True"
+            }
+          ]
+        }
+      );
+
+      const attachAdminUser = new cognito.CfnUserPoolUserToGroupAttachment(
+        this,
+        "AdminUsertoAdminGroupAttachment",
+        {
+          groupName: adminGroupName,
+          username: process.env.ADMIN_USER_EMAIL,
+          userPoolId: this.pool.userPoolId
+        }
+      );
 
       // need to make sure the admin user has been created, before
       // adding it to the admin group. This dependency makes sure
       // that happens.
       attachAdminUser.node.addDependency(adminUser);
-
     } else {
-      throw new Error('ADMIN_USER_EMAIL must be specified');
+      throw new Error("ADMIN_USER_EMAIL must be specified");
     }
 
     new cdk.CfnOutput(this, "UserPoolId", {
