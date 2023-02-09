867887af5be95832c4e5a116dbc47cc5f37c7e29
Cristian Goina <goinac@hhmi.org>
2023-02-09 16:21:31 -0500

    update jacs stack version

--- vpc_stack/src/hortacloud-services-config.ts
+++ vpc_stack/src/hortacloud-services-config.ts
@@ -1,7 +1,7 @@
 import { HortaCloudConfig, getHortaCloudConfig,
          HortaBackupConfig, getHortaBackupConfig } from '../../common/hortacloud-common';
 
-const defaultJacsStackVersion = '9.10.2';
+const defaultJacsStackVersion = '9.11.rc3';
 
 // Horta Services Config
 export interface HortaCloudServicesConfig extends HortaCloudConfig, HortaBackupConfig {
