16fed0a76e5a0cb11f5d7ecf143112bc3d3c0925
porterbot <schauderd@janelia.hhmi.org>
2025-03-10 16:46:40 -0400

    Update hortacloud-services-config.ts

--- vpc_stack/src/hortacloud-services-config.ts
+++ vpc_stack/src/hortacloud-services-config.ts
@@ -1,7 +1,7 @@
 import { HortaCloudConfig, getHortaCloudConfig,
          HortaBackupConfig, getHortaBackupConfig } from '../../common/hortacloud-common';
 
-const defaultJacsStackVersion = '9.22';
+const defaultJacsStackVersion = '9.22.1';
 
 // Horta Services Config
 export interface HortaCloudServicesConfig extends HortaCloudConfig, HortaBackupConfig {
