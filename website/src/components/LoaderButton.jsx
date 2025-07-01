cc097f743c6310844bcf9f22011f451cc06ebdf3
schauderd <schauderd@janelia.hhmi.org>
2025-07-01 15:31:31 -0400

    Added components

---
+++ website/src/components/LoaderButton.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Button } from "antd";
+
+export default function LoaderButton(props) {
+    const { loading, disabled, children } = props;
+    return (
+        <Button
+            loading={loading}
+            disabled={disabled || loading}
+            {...props} // eslint-disable-line react/jsx-props-no-spreading
+        >
+            {children}
+        </Button>
+    );
+}
+
+LoaderButton.propTypes = {
+    loading: PropTypes.bool,
+    disabled: PropTypes.bool,
+    children: PropTypes.string.isRequired
+};
+
+LoaderButton.defaultProps = {
+    loading: false,
+    disabled: false
+};
