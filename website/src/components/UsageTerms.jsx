01486ae9726f68dc7ec40469cf2a388bd37a0551
porterbot <schauderd@janelia.hhmi.org>
2025-08-14 16:47:35 -0400

    updated usage terms for Signup

--- website/src/components/UsageTerms.jsx
+++ website/src/components/UsageTerms.jsx
@@ -2,22 +2,31 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Typography } from "antd";
 
+
 const { Title } = Typography;
 
 export default function UseageTerms() {
-  return (
-    <div>
-      <Title level={3}>Usage Terms</Title>
-      <p>
-        We want to provide the very best science free for research use. In
-        return, we ask that you cite Janelia Research Campus and the following
-        publications, as applicable, should you publish using these resources:
-      </p>
-      <p>
-        We plan to periodically update the data and publications, and the latest
-        references will always be listed on the{" "}
-        <Link to="/about">About page</Link>.
-      </p>
-    </div>
-  );
+    return (
+        <div>
+            <Title level={3}>Usage Terms</Title>
+            <p>
+                The purposes of this demo website is to be able to experiment with the HortaCloud volumetric viewer
+                to view 3D Mouse Brain samples, view and experiment with annotation of neurons in these samples, and
+                get a better understanding of how the browser functions.  Because of this, user accounts are
+                limited to 7 days and will be automatically removed once they expire.  Also any annotations or work
+                done in the demo are not intended to be saved and will be periodically removed.
+            </p>
+            <p>
+                If you would like to spend a longer period of time experimenting with HortaCloud or would like to
+                interact and annotate Mouselight data, please email us at <Button
+                variant="primary"
+                as="a"
+                href="mailto:hortacloud@janelia.hhmi.org"
+            >
+                Email Us
+            </Button> and we can
+                create a user account in our Janelia HortaCloud instance.
+            </p>
+        </div>
+    );
 }
